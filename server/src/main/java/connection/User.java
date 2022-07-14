a925b377b7acb5b5c7b20fdb60e7ab3070d291b5
Arlet <artemshulga03@gmail.com>
2022-07-15 01:09:58 +0300

    Add User class for adding name for sockets

---
+++ server/src/main/java/connection/User.java
@@ -0,0 +1,24 @@
+package connection;
+
+import java.net.Socket;
+
+public class User {
+    private final Socket socket;
+    private String nickname;
+    User (String nickname, Socket socket) {
+        this.socket = socket;
+        this.nickname = nickname;
+    }
+
+    public void changeNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+}
