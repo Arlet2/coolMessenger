3c5242f89ad183deb3c6777dcf512a68a78c40ae
Arlet <artemshulga03@gmail.com>
2022-07-15 01:12:37 +0300

    Add service for packing data to dataTransferObject

---
+++ server/src/main/java/connection/data_exchanging/BasicPackageService.java
@@ -0,0 +1,30 @@
+package connection.data_exchanging;
+
+import connection_utils.DataTransferObject;
+import encryptors.EncryptorService;
+import exceptions.SerializerException;
+import utils.Serializer;
+
+public class BasicPackageService implements PackageService {
+    private final EncryptorService encryptor;
+
+    BasicPackageService(EncryptorService encryptor) {
+        this.encryptor = encryptor;
+    }
+
+    @Override
+    public DataTransferObject encryptPackageData(Object object, DataTransferObject.DataCode code)
+            throws SerializerException {
+        byte[] bytes = Serializer.convertObjectToBytes(object);
+        return generalizedPackage(encryptor.encrypt(bytes), code);
+    }
+
+    @Override
+    public DataTransferObject packageData(Object object, DataTransferObject.DataCode code) throws SerializerException {
+        return generalizedPackage(Serializer.convertObjectToBytes(object), code);
+    }
+
+    private DataTransferObject generalizedPackage(byte[] data, DataTransferObject.DataCode code) {
+        return new DataTransferObject(data, code, encryptor.getEncryptionProtocol());
+    }
+}
---
+++ server/src/main/java/connection/data_exchanging/PackageService.java
@@ -0,0 +1,9 @@
+package connection.data_exchanging;
+
+import connection_utils.DataTransferObject;
+import exceptions.SerializerException;
+
+public interface PackageService {
+    DataTransferObject encryptPackageData(Object object, DataTransferObject.DataCode code) throws SerializerException;
+    DataTransferObject packageData(Object object, DataTransferObject.DataCode code) throws SerializerException;
+}
