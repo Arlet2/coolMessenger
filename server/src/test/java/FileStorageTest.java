2c81240b115897eb3d350c821699f98e8af8ad06
Arlet <artemshulga03@gmail.com>
2022-07-21 02:17:00 +0300

    Add new test for big structures

--- server/src/test/java/FileStorageTest.java
+++ server/src/test/java/FileStorageTest.java
@@ -1,11 +1,21 @@
 import connection.data_saving.DataStorageService;
 import connection.data_saving.FileStorage;
+import encryptors.EncryptorService;
+import encryptors.EncryptorsFactory;
 import exceptions.DataSavingException;
+import exceptions.SerializerException;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Test;
+import utils.ByteString;
+import utils.Serializer;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 public class FileStorageTest {
     private static final String testFileName = "test.txt";
@@ -24,6 +34,20 @@ public class FileStorageTest {
         String result = (String) storageService.loadData();
 
         Assert.assertEquals(input, result);
+    }
+
+    @Test
+    public void bigStructureSavingTest() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add("Mummy");
+        DataStorageService storageService = new FileStorage( 1, testFileName);
+
+        storageService.saveData(list);
+
+        ArrayList<String> result = (ArrayList<String>) storageService.loadData();
+
+        Assert.assertEquals(list, result);
     }
 
 }
