9783771539f41c3c8ec0091d9f4277ebc100b6a1
Arlet <artemshulga03@gmail.com>
2022-07-21 15:40:23 +0300

    Add new constructor for exception

--- server/src/main/java/exceptions/AuthException.java
+++ server/src/main/java/exceptions/AuthException.java
@@ -4,4 +4,8 @@ public class AuthException extends Exception {
     public AuthException(String msg, Throwable cause) {
         super(msg, cause);
     }
+
+    public AuthException(String msg) {
+        super(msg);
+    }
 }
