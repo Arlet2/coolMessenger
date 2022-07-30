01a10d3a67043531d0fbd1d323e1efb730ded0cf
korlir <anonym.show@mail.ru>
2022-07-30 23:38:20 +0300

    Change usercard for only saving info about nicnames and password

--- server/src/main/java/data_saving/UserCard.java
+++ server/src/main/java/data_saving/UserCard.java
@@ -1,24 +1,12 @@
 package data_saving;
 
-import java.util.ArrayList;
-
 public class UserCard {
     private final String nickname;
     private final String passwordHash;
-    private final ArrayList<Long> availableRooms = new ArrayList<>();
 
     public UserCard(String nickname, String passwordHash) {
         this.nickname = nickname;
         this.passwordHash = passwordHash;
-    }
-
-    public void addNewRoom(long roomId) {
-        if (!availableRooms.contains(roomId))
-            availableRooms.add(roomId);
-    }
-
-    public void removeRoom(long roomId) {
-        availableRooms.remove(roomId);
     }
 
     public String getNickname() {
@@ -29,7 +17,4 @@ public class UserCard {
         return passwordHash;
     }
 
-    public ArrayList<Long> getAvailableRooms() {
-        return availableRooms;
-    }
 }
